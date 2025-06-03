7a6ee76005c1e3b0a103174a7a2bebc1d1524400
Khang000 <khangquach026@gmail.com>
2025-06-03 16:23:06 +0700

    Add navbar, sidebar, Post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from '@clerk/nextjs'
+import CreatePost from '@/components/CreatePost'
+import WhoToFollow from '@/components/WhoToFollow'
+import { currentUser } from '@clerk/nextjs/server'
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser()
+
   return (
-    <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button className="bg-red-500 ">Sign in</button>
-        </SignInButton>
-        {/* <SignUpButton /> */}
-      </SignedOut>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
 
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <div className=" hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
     </div>
   )
 }
---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,5 @@
+function ImageUpload() {
+  return <div></div>
+}
+
+export default ImageUpload
